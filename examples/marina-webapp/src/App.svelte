b8ee36f1ae05d46bc8b8a7413fb5fb11d1c50e2f
tiero <3596602+tiero@users.noreply.github.com>
2022-06-02 10:04:41 +0200

    add svelte app in /examples

---
+++ examples/marina-webapp/src/App.svelte
@@ -0,0 +1,21 @@
+<script type="ts">
+
+  import Connect from 'svelte-marina-button';
+
+  import Unlock from './Unlock.svelte';
+
+
+
+</script>
+
+<div class="container is-widescreen">
+  <div class="box">
+    <h1 class="title">Hello World!</h1>
+    <Connect cssClass={'button'} />
+    <hr />
+    <Unlock />
+  </div>
+</div>
+
+<style src="./scss/main.scss" lang="scss" global>
+</style>
