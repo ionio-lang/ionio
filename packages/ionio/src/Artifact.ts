a7b4c7f397ea2670f97e49a748e7fa0519ff93c9
Louis <louis@vulpem.com>
2022-06-02 18:43:06 +0200

    add transformArtifact function

--- packages/ionio/src/Artifact.ts
+++ packages/ionio/src/Artifact.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import { PrimitiveType } from './Argument';
+import { Argument, encodeArgument, PrimitiveType } from './Argument';
 import { Requirement } from './Requirement';
 
 interface Artifact {
@@ -20,6 +20,59 @@ interface Function {
   asm: string[];
 }
 
+type ArtifactMapper = (artifact: Artifact) => Artifact;
+
+function withConstructorInput(name: string, arg: Argument): ArtifactMapper {
+  return (artifact: Artifact): Artifact => {
+    // compile the constructor input
+    const constructorInputToRemove = artifact.constructorInputs.find(p => p.name === name);
+    if (!constructorInputToRemove) {
+      throw new Error(`Constructor input ${name} not found`);
+    }
+    const argEncoded = encodeArgument(arg, constructorInputToRemove.type);
+
+    return {
+      ...artifact,
+      // remove compiled constructor input
+      constructorInputs: artifact.constructorInputs.filter(p => p.name !== name),
+      functions: artifact.functions.map(replaceASMtoken(name, argEncoded.toString('hex'))),
+    };
+  }
+}
+
+function withCustomConstructorInputName(toreplace: string, replaceby: string): ArtifactMapper {
+  return (artifact: Artifact): Artifact => {
+      return {
+        ...artifact,
+        constructorInputs: artifact.constructorInputs.map(p => p.name === toreplace ? { ...p, name: replaceby } : p),
+        functions: artifact.functions.map(replaceASMtoken(toreplace, `$${replaceby}`)),
+      }
+  }
+}
+
+function replaceASMtoken(toreplace: string, by: string): (f: Function) => Function {
+  return (f: Function): Function => ({
+    ...f,
+    asm: f.asm.map(token => {
+      if (token.slice(1) === toreplace) {
+        return by;
+      }
+      return token;
+      }),
+    });
+  }
+
+function composeMappers(...mappers: ArtifactMapper[]): ArtifactMapper {
+  return (artifact: Artifact): Artifact => {
+    return mappers.reduce((acc, mapper) => mapper(acc), artifact);
+  };
+}
+
+function transformArtifact(artifact: Artifact, ...mappers: ArtifactMapper[]): Artifact {
+  return composeMappers(...mappers)(artifact);
+}
+
+
 function importArtifact(artifactFile: string): Artifact {
   return JSON.parse(fs.readFileSync(artifactFile, { encoding: 'utf-8' }));
 }
@@ -34,6 +87,10 @@ export {
   Function,
   Requirement,
   Parameter,
+  ArtifactMapper,
   exportArtifact,
   importArtifact,
+  transformArtifact,
+  withConstructorInput,
+  withCustomConstructorInputName,
 };
---
+++ packages/ionio/test/unit/transformer.test.ts
@@ -0,0 +1,65 @@
+import { Artifact, ArtifactMapper, withConstructorInput, withCustomConstructorInputName } from "../../src"
+
+interface Test {
+    name: string;
+    artifact: Artifact;
+    mapper: ArtifactMapper;
+    expected: Artifact;
+}
+
+const transferWithKey = require('../fixtures/transfer_with_key.json');
+
+const tests: Test[] = [
+    {
+        name: 'withConstructorInput',
+        artifact: transferWithKey,
+        mapper: withConstructorInput("pubKey", Buffer.from("c6d0c728b8e8fd24cf1a6022578f73820ba22e44fbc6de3a4b1a8b7f397fdbd1", 'hex')),
+        expected: {
+            ...transferWithKey,
+            constructorInputs: [],
+            functions: [
+                {
+                    ...transferWithKey.functions[0],
+                    asm: [
+                        "c6d0c728b8e8fd24cf1a6022578f73820ba22e44fbc6de3a4b1a8b7f397fdbd1",
+                        "OP_CHECKSIG"
+                    ]
+                }
+            ]
+        },
+    },
+    {
+        name: 'withCustomConstructorInputName',
+        artifact: transferWithKey,
+        mapper: withCustomConstructorInputName("pubKey", "pubkeycustomname"),
+        expected: {
+            ...transferWithKey,
+            constructorInputs: [
+                {
+                    ...transferWithKey.constructorInputs[0],
+                    name: "pubkeycustomname"
+                }
+            ],
+            functions: [
+                {
+                    ...transferWithKey.functions[0],
+                    asm: [
+                        "$pubkeycustomname",
+                        "OP_CHECKSIG"
+                    ]
+                }
+            ]
+        },
+    }
+
+]
+
+
+describe('transformArtifact', () => {
+    for (const t of tests) {
+        it(`should transform artifact with ${t.name}`, () => {
+            const transformed = t.mapper(t.artifact);
+            expect(transformed).toEqual(t.expected);
+        });
+    }
+})
