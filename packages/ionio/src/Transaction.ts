1aa56c012efd31cb01eb28e7ad24d95a75c25710
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2022-06-28 22:24:51 +0200

    add Liquidate path spend in Synthetic Asset contract (#17)
    * add Liquidate path spend in Synthetic Asset contract
    
    * lint

--- packages/ionio/src/Transaction.ts
+++ packages/ionio/src/Transaction.ts
@@ -282,19 +282,16 @@ export class Transaction implements TransactionInterface {
       );
     }
 
-    const encodedArgs = this.functionArgs
-      .filter(arg => !isSigner(arg))
-      .map((arg, index) => {
-        // Encode passed args (this also performs type checking)
-        return encodeArgument(
+    // check for a signature to be made
+    for (const [index, arg] of this.functionArgs.entries()) {
+      if (!isSigner(arg)) {
+        const encodedArgument = encodeArgument(
           arg,
           this.artifactFunction.functionInputs[index].type
         );
-      });
-
-    // check for a signature to be made
-    for (const arg of this.functionArgs) {
-      if (!isSigner(arg)) continue;
+        witnessStack.push(encodedArgument as Buffer);
+        continue;
+      }
 
       const signedPtxBase64 = await arg.signTransaction(this.psbt.toBase64());
       const signedPtx = Psbt.fromBase64(signedPtxBase64);
@@ -304,8 +301,11 @@ export class Transaction implements TransactionInterface {
         this.fundingUtxoIndex
       ];
       if (tapScriptSig && tapScriptSig.length > 0) {
-        witnessStack = [...tapScriptSig.map(s => s.signature), ...witnessStack];
+        for (const s of tapScriptSig) {
+          witnessStack.push(s.signature);
+        }
       } else if (tapKeySig) {
+        // is the key-path spend always first element of stack?
         witnessStack = [tapKeySig, ...witnessStack];
       }
     }
@@ -314,8 +314,7 @@ export class Transaction implements TransactionInterface {
       return {
         finalScriptSig: undefined,
         finalScriptWitness: witnessStackToScriptWitness([
-          ...witnessStack,
-          ...(encodedArgs as Buffer[]),
+          ...witnessStack.reverse(),
           input.tapLeafScript![0].script,
           input.tapLeafScript![0].controlBlock,
         ]),
--- packages/ionio/test/e2e/syntheticAsset.test.ts
+++ packages/ionio/test/e2e/syntheticAsset.test.ts
@@ -1,16 +1,27 @@
 import { Contract, Signer } from '../../src';
 import * as ecc from 'tiny-secp256k1';
-import { alicePk, bobPk, network } from '../fixtures/vars';
-import { payments, TxOutput } from 'ldk';
+import { alicePk, bobPk, oraclePk, network } from '../fixtures/vars';
+import { payments, address, TxOutput } from 'ldk';
+import crypto from 'crypto';
+import { writeUInt64LE } from 'liquidjs-lib/ts_src/bufferutils';
 import {
   broadcast,
   faucetComplex,
+  getNewAddress,
   getSignerWithECPair,
   mintComplex,
 } from '../utils';
 
+const numberToBytesLE64 = (x: number): Buffer => {
+  const buffer = Buffer.alloc(8);
+  writeUInt64LE(buffer, x, 0);
+  return buffer;
+};
+
 describe('SyntheticAsset', () => {
   const issuer = payments.p2wpkh({ pubkey: alicePk.publicKey, network })!;
+  const issuerSigner: Signer = getSignerWithECPair(alicePk, network);
+
   const borrower = payments.p2wpkh({ pubkey: bobPk.publicKey, network })!;
   const borrowerSigner: Signer = getSignerWithECPair(bobPk, network);
 
@@ -24,29 +35,46 @@ describe('SyntheticAsset', () => {
     asset: string;
   };
   let borrowUtxo: { txid: string; vout: number; value: number; asset: string };
+  let synthMintUtxo: {
+    txid: string;
+    vout: number;
+    value: number;
+    asset: string;
+  };
+  let instance: Contract;
   const borrowAmount = 500000;
   const payoutAmount = 500;
+  const feeAmount = 100;
+
+  // 40k BTCUSD
+  const priceLevel = 40000;
+  const priceLevelBytes = numberToBytesLE64(priceLevel);
+  //Tue Jun 28 2022 16:31:52 GMT+0200 (Central European Summer Time)
+  const timestamp = 1656426712;
+  const timestampBytes = numberToBytesLE64(timestamp);
 
   beforeAll(async () => {
-    // eslint-disable-next-line global-require
-
     // mint synthetic asset
     // NOTICE: this should happen as atomic swap, now we are simulating it
     // giving the borrower the asset before having him to lock collateral
+    const nigiriAddressConfidential = await getNewAddress();
+    const mintResponse = await mintComplex(
+      // we send to nigiri so we faucet for each test
+      address.fromConfidential(nigiriAddressConfidential).unconfidentialAddress,
+      // we mint three times the borrow amount for each test
+      (borrowAmount * 3) / 10 ** 8
+    );
+    synthMintUtxo = mintResponse.utxo;
+  });
+
+  beforeEach(async () => {
     try {
-      const mintResponse = await mintComplex(
-        borrower.address!,
-        borrowAmount / 10 ** 8
-      );
-      borrowUtxo = mintResponse.utxo;
-      borrowPrevout = mintResponse.prevout;
-      // instantiate Contract
       const artifact = require('../fixtures/synthetic_asset.json');
       contract = new Contract(
         artifact,
         [
           // borrow asset
-          borrowUtxo.asset,
+          synthMintUtxo.asset,
           // collateral asset
           network.assetHash,
           // borrow amount
@@ -54,61 +82,125 @@ describe('SyntheticAsset', () => {
           // payout on redeem amount for issuer
           payoutAmount,
           borrower.pubkey!.slice(1),
+          oraclePk.publicKey.slice(1),
           issuer.pubkey!.slice(1),
           issuer.output!.slice(2), // segwit program
+          priceLevelBytes,
+          timestampBytes,
         ],
         network,
         ecc
       );
-
-      // fund our contract
-      const faucetResponse = await faucetComplex(contract.address, 0.0001);
-      covenantPrevout = faucetResponse.prevout;
-      covenantUtxo = faucetResponse.utxo;
     } catch (e) {
       console.error(e);
     }
+
+    // 1. send synthetic asset to borrower
+    const faucetSynthResponse = await faucetComplex(
+      borrower.address!,
+      borrowAmount / 10 ** 8,
+      synthMintUtxo.asset
+    );
+    borrowUtxo = faucetSynthResponse.utxo;
+    borrowPrevout = faucetSynthResponse.prevout;
+
+    // 2. fund our contract with collateral
+    const faucetCollateralResponse = await faucetComplex(
+      contract.address,
+      0.0001
+    );
+    covenantPrevout = faucetCollateralResponse.prevout;
+    covenantUtxo = faucetCollateralResponse.utxo;
+
+    // 3. lets instantiate the contract using the funding transacton
+    instance = contract.from(
+      covenantUtxo.txid,
+      covenantUtxo.vout,
+      covenantPrevout
+    );
   });
 
-  describe('redeem', () => {
-    it('should redeem with burnt output', async () => {
-      const feeAmount = 100;
+  it('should redeem with burnt output', async () => {
+    const tx = instance.functions
+      .redeem(borrowerSigner)
+      // spend an asset
+      .withUtxo({
+        txid: borrowUtxo.txid,
+        vout: borrowUtxo.vout,
+        prevout: borrowPrevout,
+      })
+      // burn asset
+      .withOpReturn(borrowUtxo.value, borrowUtxo.asset)
+      // payout to issuer
+      .withRecipient(issuer.address!, payoutAmount, network.assetHash)
+      // collateral
+      .withRecipient(
+        borrower.address!,
+        covenantUtxo.value - payoutAmount - feeAmount,
+        network.assetHash
+      )
+      .withFeeOutput(feeAmount);
 
-      // lets instantiate the contract using the funding transacton
-      const instance = contract.from(
-        covenantUtxo.txid,
-        covenantUtxo.vout,
-        covenantPrevout
-      );
+    const partialSignedTx = await tx.unlock();
 
-      const tx = instance.functions
-        .redeem(borrowerSigner)
-        // spend an asset
-        .withUtxo({
-          txid: borrowUtxo.txid,
-          vout: borrowUtxo.vout,
-          prevout: borrowPrevout,
-        })
-        // burn asset
-        .withOpReturn(borrowUtxo.value, borrowUtxo.asset)
-        // payout to issuer
-        .withRecipient(issuer.address!, payoutAmount, network.assetHash)
-        // collateral
-        .withRecipient(
-          borrower.address!,
-          covenantUtxo.value - payoutAmount - feeAmount,
-          network.assetHash
-        )
-        .withFeeOutput(feeAmount);
+    const signedTx = await partialSignedTx.psbt.signInput(1, bobPk);
+    const finalizedTx = await signedTx.finalizeInput(1);
 
-      const partialSignedTx = await tx.unlock();
+    const hex = finalizedTx.extractTransaction().toHex();
+    const txid = await broadcast(hex);
+    expect(txid).toBeDefined();
+  });
 
-      const signedTx = await partialSignedTx.psbt.signInput(1, bobPk);
-      const finalizedTx = await signedTx.finalizeInput(1);
+  it('should liquidate with oracle attestation', async () => {
+    // 30k BTCUSD
+    const underwaterPrice = 30000;
+    const underwaterPriceBytes = numberToBytesLE64(underwaterPrice);
 
-      const hex = finalizedTx.extractTransaction().toHex();
-      const txid = await broadcast(hex);
-      expect(txid).toBeDefined();
-    });
+    // Tue Jun 28 2022 17:11:55 GMT+0200 (Central European Summer Time)
+    const timestampNow = 1656429115;
+    const timestampNowBytes = numberToBytesLE64(timestampNow);
+
+    const message = Buffer.from([
+      ...timestampNowBytes,
+      ...underwaterPriceBytes,
+    ]);
+    const hash = crypto
+      .createHash('sha256')
+      .update(message)
+      .digest();
+    const oracleDataSig = oraclePk.signSchnorr(hash);
+
+    const tx = instance.functions
+      .liquidate(
+        underwaterPriceBytes,
+        timestampNowBytes,
+        oracleDataSig,
+        issuerSigner
+      )
+      // spend an asset
+      .withUtxo({
+        txid: borrowUtxo.txid,
+        vout: borrowUtxo.vout,
+        prevout: borrowPrevout,
+      })
+      // burn asset
+      .withOpReturn(borrowUtxo.value, borrowUtxo.asset)
+      // confiscate collateral
+      .withRecipient(
+        issuer.address!,
+        covenantUtxo.value - feeAmount,
+        network.assetHash
+      )
+      .withFeeOutput(feeAmount);
+
+    const partialSignedTx = await tx.unlock();
+
+    // sign and finalize the synth asset input
+    const signedTx = await partialSignedTx.psbt.signInput(1, bobPk);
+    const finalizedTx = await signedTx.finalizeInput(1);
+
+    const hex = finalizedTx.extractTransaction().toHex();
+    const txid = await broadcast(hex);
+    expect(txid).toBeDefined();
   });
 });
