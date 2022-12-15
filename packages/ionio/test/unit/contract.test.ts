8b2f91d194292e3d0c84da738a3a6b64bdcfc116
tiero <3596602+tiero@users.noreply.github.com>
2022-12-15 19:41:12 +0100

    lint

--- packages/ionio/test/unit/contract.test.ts
+++ packages/ionio/test/unit/contract.test.ts
@@ -7,20 +7,14 @@ import { Contract } from '../../src';
 const transferWithKey = require('../fixtures/transfer_with_key.json');
 
 describe('contract', () => {
-  
   it('should have contractParams not empty and with the same keys as constructorInputs', async () => {
     const zkp = await secp256k1();
 
     const pubkey = `0x${alicePk.publicKey.slice(1).toString('hex')}`;
-    const contract = new Contract(
-      transferWithKey,
-      [pubkey],
-      network,
-      {
-        zkp,
-        ecc
-      }
-    );
+    const contract = new Contract(transferWithKey, [pubkey], network, {
+      zkp,
+      ecc,
+    });
 
     expect(contract.contractParams['pubKey']).toStrictEqual(pubkey);
   });
